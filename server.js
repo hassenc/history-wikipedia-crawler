100e8d15a400bc7d17dbf5314a1e2614a9d26a25
Hassen <chaieb.hassen@yahoo.com>
2015-04-30 23:26:24 -0700

    regex

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var Crawler = require("simplecrawler");
 var fs = require('fs');
 
 
-var crawler = Crawler.crawl("http://en.wikipedia.org/wiki/910s_BC");
+var crawler = Crawler.crawl("http://en.wikipedia.org/wiki/820s_BC");
 
 // crawler.interval = 10000;
 crawler.maxConcurrency = 1;
@@ -13,31 +13,77 @@ crawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
     console.log("I just received %s (%d bytes)", queueItem.url, responseBuffer.length);
     console.log("It was a resource of type %s", response.headers['content-type']);
     body = responseBuffer.toString();
-    // var re = new RegExp("^([a-z0-9]*)$");
-    // // var re = /(li \d+(\.\d)*)/i;
-    // var aaa=body.match(/^([a-z0-9]{5,})$/g)
 
-var myString = 'aaa1zzz 81zzz 81ccc81zzz8aaa ttrtttrytrhunuybuu yu yu uy uytut';
-var myRegexp = /<h2>.*?Events and trends.*?<\/h2>\n<ul>\n(?:<li(.*?)<\/li>\n)*<\/ul>/gim;
-var regx = /aaa(1.*?8)*aaa/gmi;
-var match =["aaa"];
-var str="";
-while (match !== null) {
-	match = regx.exec(myString);
-	if (match) {
-		str =str + JSON.stringify(match);
-		console.log(match)
-		// console.log(match[1])
-	}
-	console.log('--------------------')
-}
+    var getEventsBlock = function(myString) {
+        // var myRegexp = /<h2>.*?Events.*?<\/h2>(?:.|\s)*?895 BC: Death of /gim;
+        var myRegexp = /<h2>.*?Events.*?<\/h2>\n(?:.|\s)*?(?:<h3>.*?By place.*?<\/h3>\n)?((?:(?:<h4>.*?<\/h4>\n)?<ul>\n(?:<li.*?<\/li>\n)*<\/ul>\n)+)/gim;
 
-fs.writeFile("test", body, function(err) {
+        // var myRegexp = /<h2>.*?Events.*?<\/h2>\n(?:(?:.|\s)*?)?((?:<h3>.*?By place.*?|(?:.|\s)*?BC(?:.|\s)*?<\/h3>\n)?(?:(?:<h4>.*?<\/h4>\n)?<ul>\n(?:<li.*?<\/li>\n)*<\/ul>\n)*)/gim;
+        // var myRegexp = /<h2>.*?Events.*?<\/h2>(?:.|\s)*?<h3(?:.|\s)*?BC(?:.|\s)*?<\/h3>/gim;
+        var match = myRegexp.exec(myString);
+        // console.log(match[2])
+        console.log('.............')
+        // console.log(match[0])
+        // console.log(match[1])
+        return match[1];
+    };
+
+    var getEventsFromBlock = function(eventsBlock) {
+        var myRegexp = /(?:<h4>.*?<span.*?>(.*?)<\/span>.*?<\/h4>\n)?<ul>\n((?:<li.*?<\/li>\n)*)<\/ul>\n/gmi;
+        var match = "";
+        while (match !== null) {
+            match = myRegexp.exec(eventsBlock);
+            if (match) {
+                if(match[1]) {
+                    console.log("With title : ",match[1]);
+                } 
+                getEventsFromUl(match[2]);
+                
+            }
+        }
+    };
+
+        var str ="";
+    var getEventsFromUl = function(eventsBlock) {
+        var myRegexp = /<li>(.*?)<\/li>/gmi;
+        var match = "";
+        while (match !== null) {
+         match = myRegexp.exec(eventsBlock);
+         if (match) {
+             str =str + match[1] + '\n\n\n';
+             console.log(match[1])
+         }
+         console.log('--------------------')
+        }
+    };
+    var eventsBlock = getEventsBlock(body);
+    getEventsFromBlock(eventsBlock);
+
+// var myString = '1zzz 81zzz 81ccc81zzz8';
+
+// var regx = /1(.*?)8/gmi;
+// var match =["aaa"];
+// var str="";
+    
+// while (match !== null) {
+// 	match = regx.exec(myString);
+// 	if (match) {
+// 		str =str + JSON.stringify(match);
+// 		console.log(match)
+// 		// console.log(match[1])
+// 	}
+// 	console.log('--------------------')
+// }
+
+fs.writeFile("test", str, function(err) {
     if(err) {
         return console.log(err);
     }
 
     console.log("The file was saved!");
 });
+
+
+
 
 });
